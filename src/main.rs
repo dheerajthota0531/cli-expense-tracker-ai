a44f9653601462f6ceec5b579c05b556656745e9
dheerajthota0531 <dheeraj.thota.0531@gmail.com>
2026-05-27 12:31:56 +0530

    Added Record Type and Enchanced Summary

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,22 @@ use std::{
 };
 
 use std::collections::HashMap;
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "lowercase")]
+enum RecordType{
+    
+    Income,
+    Expense,
+}
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 struct Record {
+     #[serde(rename = "type")]
+    spend_type:RecordType,
     amount:f64,
     category: String,
     description: String,
+
 
 }
 
@@ -19,6 +29,7 @@ fn main() {
     println!("Welcome To Expense Tracker-Pro");
 
     loop {
+
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("falied to read");
         let input = input.trim();
@@ -55,18 +66,47 @@ fn main() {
 fn summary() -> Result<(), Box<dyn Error>>{
     let mut sum:HashMap< String, f64 > = HashMap::new();
     let file = File::open("expenses.csv")?;
+    let mut total_income = 0.0;
+    let mut total_expense = 0.0;
+
     let mut  rdr = csv::Reader::from_reader(file);
     for result in rdr.deserialize() {
         let record:Record = result?;
-        let total = sum.entry(record.category).or_insert(0.0);
-         *total += record.amount;
+        match record.spend_type {
+            RecordType::Expense => {
+                total_expense += record.amount;
+            }
+            RecordType::Income => {
+                total_income += record.amount;
+
+                let total = sum.entry(record.category).or_insert(0.0);
+                *total += record.amount;
+            }
+        } 
+    }
+    println!("{}", "++".repeat(30));
+    let net_bal = total_income - total_expense;
+        
+    println!("Total Income   => {:.2}", total_income);
+    println!("Total Expense  => {:.2}", total_expense);
+    println!("Net Balance    => {:.2}", net_bal);
+
+     println!("{}", "++".repeat(15));
+
+
+    if net_bal < 0.0 {
+        println!("Status         => Deficit");
+    } else {
+        println!("Status         => surplus");
     }
 
-
+     println!("{} Category {}", "++".repeat(15),  "++".repeat(15));
     for (category, amount) in sum {
         println!("{} => {}", category, amount);
         
     }
+
+    println!("{}", "++".repeat(30));
     
     Ok(())
 }
@@ -75,10 +115,24 @@ fn summary() -> Result<(), Box<dyn Error>>{
 fn list_expenses() -> Result<(), Box<dyn Error>> {
     let file = File::open("expenses.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
+    println!("\n{:<10} {:<10} {:<15} ", "Type, Amount", "Category", "Description");
+    let mut total = 0.0;
+    let mut count = 0;
+    println!("{}", "-".repeat(50));
     for result in rdr.deserialize() {
         let record:Record = result?;
-        println!("{:?}", record);
+       println!("{:<?} {:<10.2} {:<15} {}", record.spend_type,record.amount, record.category, record.description);
+       total += record.amount;
+       count += 1;
     }
+
+    if count == 0{
+        println!("No Expenses");
+    }else{
+        println!("{}", "-".repeat(50));
+        println!("{:<10.2} ({count} expenses)", total);
+    }
+
     Ok(())
 }
 
@@ -86,9 +140,23 @@ fn list_expenses() -> Result<(), Box<dyn Error>> {
 fn add_expenses() -> Result<(), Box<dyn Error>> {
     let file = OpenOptions::new().append(true).create(true).open("expenses.csv")?;
     let mut wtr = csv::WriterBuilder::new()
+
         .delimiter(b',')
         .has_headers(false)
         .from_writer(file);
+
+    let mut spend = String::new();
+    println!("Enter Type: Income/Expense");
+    io::stdin().read_line(&mut spend).expect("failed to read input");
+    let spend_type = match spend.trim() {
+        "Income" => RecordType::Income,
+        "Expense" => RecordType::Expense,
+        _ => {
+            println!("Choose: Income or Expense");
+            return Ok(())
+        }
+    };
+
 
     let mut  expense_amout = String::new();
     println!("Enter amout");
@@ -106,6 +174,7 @@ fn add_expenses() -> Result<(), Box<dyn Error>> {
     let desc = expense_description.trim().to_string();
 
     let  new_expense = Record{
+        spend_type:spend_type,
         amount:amt,
         category:cate,
         description:desc,
@@ -113,6 +182,8 @@ fn add_expenses() -> Result<(), Box<dyn Error>> {
 
     wtr.serialize(new_expense)?;
     wtr.flush()?;
+   
+    println!("Added successfully" );
     
     Ok(())
 }
